391db4dfe6156e6875bfdad553fa3943eeae7a0b
wx1208 <wx6611210@163.com>
2019-10-19 15:15:50 +0800

    地图显示

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>hotel</div>
+    <div>
+      <h3>高德地图</h3>
+      <div id="container"></div>
+    </div>
 </template>
 
 <script>
 export default {
-
+  // 574d7ff83aafc901e76827057406372d
+mounted(){
+   window.onLoad = function () { var map = new AMap.Map('container'); } 
+   var url = 'https://webapi.amap.com/maps?v=1.4.15&key=574d7ff83aafc901e76827057406372d&callback=onLoad'; 
+   var jsapi = document.createElement('script'); 
+   jsapi.charset = 'utf-8'; jsapi.src = url; document.head.appendChild(jsapi);
+}
+ 
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+#container {width:300px; height: 180px; }
 
 </style>
